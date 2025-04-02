cafc31e84585a31894c22bdbc8de0710add379c7
dorothiakantapia <67118524+dorothiakantapia@users.noreply.github.com>
2025-04-02 14:25:16 -0700

    Delete scripts directory

--- scripts/quality_check_fastqc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sample_id=$1
-input_dir="/scratch/spark323/trimmed"
-output_dir="/scratch/spark323/trimmed"
-
-fastqc ${input_dir}/${sample_id}_R1_trimmed.fastq.gz \
-       ${input_dir}/${sample_id}_R2_trimmed.fastq.gz \
-       --outdir=${output_dir}
